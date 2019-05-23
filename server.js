61440bc2dd419a468439123a8930256977774517
Moshe Praver <moshepraver@gmail.com>
2019-05-23 14:07:19 -0500

    first 2 routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
+var dns = require('dns');
 
 var cors = require('cors');
 
@@ -12,12 +14,24 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI);
+
+var websiteSchema = mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url : {
+    type: Number,
+    // required: true
+  } 
+})
+
+var Website = mongoose.model('Website', websiteSchema);
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -30,6 +44,36 @@ app.get('/', function(req, res){
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+app.post('/api/shorturl/new', async (req, res) => {
+  let counter;
+  await Website.countDocuments({}, (err, count) => {
+    if (err) console.log('error in count')
+    console.log({count})
+    counter = count;
+    console.log({counter, count})
+  });
+  console.log(counter)
+  var website = new Website ({ 
+    original_url: req.body.url,
+    short_url: counter
+  });
+  website.save(function (err) {
+  if (err) console.log(err);
+  });
+  res.json({
+    original_url: req.body.url,
+    short_url: counter
+  })
+})
+
+app.get('/api/shorturl/:short_url', async (req, res) => {
+  await Website.find({short_url: Number(req.params.short_url)}, (err, website) => {
+    err ? res.send('error') 
+    : res.redirect(website[0].original_url)
+  })
+  
+})
 
 
 app.listen(port, function () {
