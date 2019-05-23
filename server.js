8c8caed2873e2a5a1533d25fe328e3e0249be590
Moshe Praver <moshepraver@gmail.com>
2019-05-23 14:10:27 -0500

    clean up

--- server.js
+++ server.js
@@ -30,19 +30,11 @@ var websiteSchema = mongoose.Schema({
 var Website = mongoose.model('Website', websiteSchema);
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
-});
-
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
 });
 
 app.post('/api/shorturl/new', async (req, res) => {
